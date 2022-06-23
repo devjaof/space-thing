277f4f5950cadc44dd388906c4b8938637e9b857
Joao Francisco <joao.rodrigues@viasoft.com.br>
2022-06-23 15:15:28 -0300

    commit

--- main.js
+++ main.js
@@ -49,9 +49,8 @@ function addStar() {
 
 Array(500).fill().forEach(addStar);
 
-// const spaceTexture = new THREE.TextureLoader().load('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/95629f34144463.56c58b1959576.jpg');
-const colorBlack = new THREE.Color( 0x0000000 );
-scene.background = colorBlack;
+const spaceTexture = new THREE.TextureLoader().load('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/95629f34144463.56c58b1959576.jpg');
+scene.background = spaceTexture;
 
 camera.position.z = 100;
 
