070a3b9bbb250912f98acc74d1b67058ebc8eb1f
João Francisco Rodrigues <joao.rodrigues@viasoft.com.br>
2023-06-09 17:38:20 -0300

    feat: pickle rick is on!

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-import './style.css'
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
 
-import { OrbitControls }from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 // set up
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -20,62 +19,78 @@ document.body.appendChild(renderer.domElement);
 
 camera.position.z = 80;
 
-let pickle;
+const _GLTFLoader = new GLTFLoader();
 
-new GLTFLoader().load(
-  './picklerick.gltf',
-  function ( object ) {
-    pickle = object.scene;
-      scene.add( pickle );
+const pickle = _GLTFLoader.load(
+  "./picklerick.gltf",
+  function (gltf) {
+    scene.add(gltf.scene);
+
+    gltf.animations; // Array<THREE.AnimationClip>
+    gltf.scene; // THREE.Group
+    gltf.scenes; // Array<THREE.Group>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
   },
-  function ( xhr ) {
-      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
   },
-  function ( error ) {
-      console.log('An error happened: ', error );
+  function (error) {
+    console.log("An error happened: ", error);
   }
 );
+
+// material
+const torus = new THREE.TorusKnotGeometry(20, 6, 50, 150);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x8318b4,
+});
+
+const torusKnot = new THREE.Mesh(torus, material);
+// scene.add(torusKnot);
 
 //star
 function addStar() {
   const geometry = new THREE.SphereGeometry(1, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(500));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(500));
 
   star.position.set(x, y, z);
   scene.add(star);
-};
+}
 Array(500).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/95629f34144463.56c58b1959576.jpg');
+const spaceTexture = new THREE.TextureLoader().load(
+  "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/95629f34144463.56c58b1959576.jpg"
+);
 scene.background = spaceTexture;
 
-
 // light
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
-const pointLight = new THREE.PointLight({color: 0xffffff});
+const pointLight = new THREE.PointLight({ color: 0xffffff });
 pointLight.position.set(0, -40, 0);
 scene.add(pointLight, ambientLight);
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
 
 // const lightHelper = new THREE.PointLightHelper(pointLight);
 // scene.add( lightHelper);
 
-
 // controls
 const controls = new OrbitControls(camera, renderer.domElement);
-
 
 function animate() {
   requestAnimationFrame(animate);
 
-  if(pickle) pickle.rotation.y += 0.005;
+  // if (torus) {
+  //   torus.rotation.y += 0.05;
+  // }
 
   controls.update();
 
